ba1a10024ee7d04b53c979489e307c8235167f77
ANG3L-GAMBOA <angelxdangerxdgamara@gmail.com>
2023-08-09 18:06:53 -0500

    edition final

--- index.js
+++ index.js
@@ -1,88 +1,92 @@
 const image = document.getElementById('cover'),
-title = document.getElementById('music-title'),
-artist = document.getElementById('music-artist'),
-currentTimeEl = document.getElementById('current-time'),
-durationEl = documentc.getElementById('duration'),
-progress = document.getElementsById('progress')
-playerProgress = document.getElementById('player-progress'),
-prevBtn = document.getElementById('prev'),
-nextBtn = document.getElementById('next'),
-playBtn = document.getElementById('play'),
-background = document.getElementById('bg-img');
+    title = document.getElementById('music-title'),
+    artist = document.getElementById('music-artist'),
+    currentTimeEl = document.getElementById('current-time'),
+    durationEl = document.getElementById('duration'),
+    progress = document.getElementById('progress'),
+    playerProgress = document.getElementById('player-progress'),
+    prevBtn = document.getElementById('prev'),
+    nextBtn = document.getElementById('next'),
+    playBtn = document.getElementById('play'),
+    background = document.getElementById('bg-img');
 
 const music = new Audio();
 
 const songs = [
     {
-    path: 'assets/1.mp3',
-    displayName: 'the Charmer\'s Call',
-    cover:'assets/1.jpg',
-    artist: 'Hanu Dixit',
+        path: 'assets/1.mp3',
+        displayName: 'The Charmer\'s Call',
+        cover: 'assets/1.jpg',
+        artist: 'Hanu Dixit',
     },
     {
         path: 'assets/2.mp3',
-        displayName: 'You will Never See Me Coming',
-        cover:'assets/1.jpg',
+        displayName: 'You Will Never See Me Coming',
+        cover: 'assets/2.jpg',
         artist: 'NEFFEX',
     },
     {
         path: 'assets/3.mp3',
         displayName: 'Intellect',
-        cover:'assets/1.jpg',
-        artist: 'Young Logos',
-    }, 
+        cover: 'assets/3.jpg',
+        artist: 'Yung Logos',
+    }
 ];
 
 let musicIndex = 0;
 let isPlaying = false;
 
-function tooglePlay(){
-    if(isPlaying){
+function togglePlay() {
+    if (isPlaying) {
         pauseMusic();
-    }else{
+    } else {
         playMusic();
     }
 }
 
-function playMusic(){
-    isPlaying =true;
-    //change play button icon
+function playMusic() {
+    isPlaying = true;
+    // Change play button icon
     playBtn.classList.replace('fa-play', 'fa-pause');
-    //Set button hover title
-    playBtn.setAttribute('title','pause');
+    // Set button hover title
+    playBtn.setAttribute('title', 'Pause');
     music.play();
 }
 
-function pauseMusic(){
-    isPlaying = flase;
-    //change pause button icon
+function pauseMusic() {
+    isPlaying = false;
+    // Change pause button icon
     playBtn.classList.replace('fa-pause', 'fa-play');
-    //set button hover title
+    // Set button hover title
     playBtn.setAttribute('title', 'Play');
     music.pause();
 }
-function loadMusic(song){
+
+function loadMusic(song) {
     music.src = song.path;
     title.textContent = song.displayName;
     artist.textContent = song.artist;
     image.src = song.cover;
     background.src = song.cover;
 }
-function changeMusic(diretion){
+
+function changeMusic(direction) {
     musicIndex = (musicIndex + direction + songs.length) % songs.length;
     loadMusic(songs[musicIndex]);
     playMusic();
 }
-function updateProgressBar(){
+
+function updateProgressBar() {
     const { duration, currentTime } = music;
     const progressPercent = (currentTime / duration) * 100;
-    progress.style.width= `${progressPercent}%`;
+    progress.style.width = `${progressPercent}%`;
 
     const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
     durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
-    currentTimeEl.textContent= `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
 }
-function setPrgressBar(e){
+
+function setProgressBar(e) {
     const width = playerProgress.clientWidth;
     const clickX = e.offsetX;
     music.currentTime = (clickX / width) * music.duration;
@@ -93,6 +97,6 @@ prevBtn.addEventListener('click', () => changeMusic(-1));
 nextBtn.addEventListener('click', () => changeMusic(1));
 music.addEventListener('ended', () => changeMusic(1));
 music.addEventListener('timeupdate', updateProgressBar);
-playerProgress.addEventListener('click', StylePropertyBar);
+playerProgress.addEventListener('click', setProgressBar);
 
-loadMusic(songs[musicIndex]);           
+loadMusic(songs[musicIndex]);
